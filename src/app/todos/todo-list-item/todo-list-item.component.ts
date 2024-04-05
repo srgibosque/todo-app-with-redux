1e1da47d081ae70de318441226e32b1fbabb139d
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-04-05 11:52:08 +0200

    todo model definition

---
+++ src/app/todos/todo-list-item/todo-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-item',
+  templateUrl: './todo-list-item.component.html',
+  styleUrl: './todo-list-item.component.css'
+})
+export class TodoListItemComponent {
+
+}
--- src/app/todos/todo.module.ts
+++ src/app/todos/todo.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodoListComponent,
+    TodoListItemComponent,
+    TodoAddComponent
+  ],
   imports: [
     CommonModule
   ]
