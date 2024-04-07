e9936fb4768e5e8b395ffe268de04fb92b2f047b
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-04-07 21:10:49 +0200

    functions edit/delete/complete readapted for the new State

--- src/app/todos/reducers/todo.reducer.ts
+++ src/app/todos/reducers/todo.reducer.ts
@@ -26,25 +26,46 @@ const _todoReducer = createReducer(
     loaded: false,
     todos: [...state.todos, new TodoDTO(title)]
   })),
-  // on(actions.completeTodo, (state, {id}) => {
-  //   return state.map((todo) => {
-  //     if(todo.id === id) {
-  //       return {...todo, done: true};
-  //     } else {
-  //       return todo;
-  //     }
-  //   })
-  // }),
-  // on(actions.editTodo, (state, {id,title}) => {
-  //   return state.map((todo) => {
-  //     if(todo.id === id){
-  //       return {...todo, title: title};
-  //     } else {
-  //       return todo;
-  //     }
-  //   })
-  // }),
-  // on(actions.deleteTodo, (state, {id}) => state.filter(todo => todo.id !== id))
+
+  on(actions.completeTodo, (state, {id}) => ({
+    ...state,
+    loading: false,
+    loaded: false,
+    todos: [...state.todos.map((todo) => {
+      if(todo.id === id){
+        return {
+          ...todo,
+          done: true,
+        }
+      } else {
+        return todo;
+      }
+    })]
+  })),
+
+  on(actions.editTodo, (state, {id,title}) => ({
+    ...state,
+    loading: false,
+    loaded: false,
+    todos: [...state.todos.map((todo) => {
+      if(todo.id === id){
+        return {
+          ...todo,
+          title
+        }
+      } else {
+        return todo;
+      }
+    })]
+  })),
+
+  on(actions.deleteTodo, (state, {id}) => ({
+    ...state,
+    loading: false,
+    loaded: false,
+    todos: [...state.todos.filter((todo) => todo.id !== id)]
+
+  }))
 );
 
 export function todoReducer(state: TodoState | undefined , action: Action){
